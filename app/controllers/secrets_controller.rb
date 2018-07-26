9a381491588290fda96df6bce6257d49b83c99da
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-07-26 11:51:41 -0400

    pre rubocop

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,18 @@
+class SecretsController < ApplicationController
+before_action :require_login
+
+
+
+  def show
+    render 'secrets/show'
+  end
+
+
+
+
+private
+
+  def require_login
+    redirect_to controller: 'sessions', action: 'new' unless session.include? :name
+  end
+end
